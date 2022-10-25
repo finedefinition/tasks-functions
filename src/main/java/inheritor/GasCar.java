ea47ebfec275bd18093127d3078d12d52caf4d2a
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-10-25 10:53:32 +0100

    tasks added

---
+++ src/main/java/inheritor/GasCar.java
@@ -0,0 +1,8 @@
+package inheritor;
+
+public class GasCar extends Car {
+    public GasCar() {
+super("Gas Car");
+    }
+
+}
---
+++ src/main/java/inheritor/HybridCar.java
@@ -0,0 +1,7 @@
+package inheritor;
+
+public class HybridCar extends Car {
+    public HybridCar() {
+super("Hybrid Car");
+    }
+}
